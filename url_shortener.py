c94d3e753d8b5a41e051a526b78dd2f41dae25ef
faizaan046 <134759918+faizaan046@users.noreply.github.com>
2023-06-04 01:42:05 +0530

    Update url_shortener.py

--- url_shortener.py
+++ url_shortener.py
@@ -15,20 +15,19 @@ def shorten_link(full_link,link_name):
         title= data['url']['title']
         short_link= data['url']['shortLink']
 
-        print("Title:",title)
-        print("Link:",short_link)
+        print("Title: ",title)
+        print("Link: ",short_link)
 
     except:
         status = data['url']['status']
-        print("Error Status", status)
+        print("Error Status: ", status)
         # url => status: 1	the shortened link comes from the domain that shortens the link, i.e. the link has already been shortened
         # url => status: 2	the entered link is not a link
         # url => status: 3	the preferred link name is already taken
         # url => status: 4	Invalid API key
         # url => status: 5	the link has not passed the validation. Includes invalid characters
         # url => status: 6	The link provided is from a blocked domain
-        # url => status: 7	OK - the link has been shortened
-        # url => status: 8	You have reached your monthly link limit. You can upgrade your subscription plan to add more links.
+        # url => status: 7	OK - the link has been shortened.
 
 link= input("Enter URL: ")
 name= input("Enter name: ")
